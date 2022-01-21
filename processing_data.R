8fee7e20a5ca00126e7d45c3c414c2e6bd590321
Bernard Tembo <ben2great@yahoo.co.uk>
2022-01-21 07:49:51 +0200

    a commit after ploting the initial comparision graph.

--- processing_data.R
+++ processing_data.R
@@ -11,103 +11,12 @@ library(tidyverse, warn.conflicts = FALSE)
 setwd("~/Documents/modelling/Energy Modelling/cleaning_energy_balance") # Note: this will be different on your computer
 
 
-## Paths to the location of raw data
-esnon_file <- "./raw_eb_data/Zambia_Energy_Balances_esnon.xlsx"
-eeb_file <- "./raw_eb_data/Zambia_ExtendedEB_1990-.csv"
-
-
-# Input the raw data from esnon
-combine_esnon_data <- function(InputDataPath){
-  
-  dataVal_flow <- df_oed_flow %>% 
-    filter(!is.na(flow_esnon))
-  # df_esnon$PRODUCT == dfx1$flow_esnon
-  
-  file_path <- InputDataPath
-  
-  sheets <- excel_sheets(path = file_path)
-  
-  print(sheets)
-  
-  for (sheet in sheets){
-    
-    dataVal <- readxl::read_xlsx(file_path,
-                                 sheet = sheet,
-                                 skip = 6,
-                                 col_names = T) %>%
-      # dplyr::mutate(source = "esnon", # Include the source of the data
-      #               year = sheet) %>% 
-      dplyr::filter(PRODUCT %in% df_oed_flow$flow_esnon,
-                    !is.na(PRODUCT)) %>%
-      dplyr::mutate(source = "esnon", # Include the source of the data
-                    year = sheet,
-                    flow_index = dataVal_flow$flow_esnon_eeb_index)
-    
-    print(dataVal)
-    
-  }
-  
-  return(dataVal)
-  
-} # Continue woeking on this function # Look out for the error under the row with BKB etc
-
-df_esnon <- combine_esnon_data(esnon_file)
-
-
-
-
-length(unique(df_esnon$PRODUCT))
-length(unique(df_oed_flow$flow_esnon))
-
-
-dfx1 <- df_oed_flow %>% 
-  filter(!is.na(flow_esnon))
-
-
-df_esnon$PRODUCT == dfx1$flow_esnon
-
-df <- df_esnon %>% 
-  tidyr::gather("Product", "value", -PRODUCT, -source, -year, -flow_index)
-  
-df
-
-
-
-#Input the raw data
-df_eeb <- read_csv(eeb_file,
-                   show_col_types = FALSE,
-                   col_names = T) %>%
-  dplyr::mutate(source = "eeb") # Include the source of the data
-
-df_eeb
-
-
-unique(df_eeb$'Flag Codes')
-unique(df_eeb$Unit)
-
-length(sort(unique(df_eeb$Product))) == length(sort(unique(df_eeb$Flow)))
-
-
-## get the names of the products in the dataset
-# df_raw %>% 
-#   dplyr::select(PRODUCT, Product) %>% 
-#   dplyr::distinct() -> df_prod
-# 
-# write.csv(df_prod, 'eb_products.csv', row.names = F)
-
-
-## get the names of the flow in the dataset
-# df_raw %>%
-#   dplyr::select(FLOW, Flow) %>%
-#   dplyr::distinct() -> df_flow
-# 
-# write.csv(df_flow, 'eb_flow.csv', row.names = F)
-
+##
 
 # Input other data that will be used to process the energy balance
 df_oed_flow <- readxl::read_xlsx("other_energy_data.xlsx",
-                             sheet = "flow",
-                             col_names = T) %>% 
+                                 sheet = "flow",
+                                 col_names = T) %>% 
   dplyr::distinct()
 
 df_oed_flow
@@ -116,19 +25,32 @@ df_oed_flow
 
 
 df_oed_prod <- readxl::read_xlsx("other_energy_data.xlsx",
-                             sheet = "product",
-                             col_names = T) %>%
+                                 sheet = "product",
+                                 col_names = T) %>%
   dplyr::distinct()
 
 df_oed_prod
 # 
 # length(sort(unique(df_oed_prod$Flow)))
 
+df_conv_factors <- readxl::read_xlsx("other_energy_data.xlsx",
+                                     sheet = "conversion_factors",
+                                     col_names = T) %>%
+  dplyr::select(-source) %>% 
+  dplyr::distinct()
+
+df_conv_factors
 
 
+## Paths to the location of raw data
+esnon_file <- "./raw_eb_data/Zambia_Energy_Balances_esnon.xlsx"
+eeb_file <- "./raw_eb_data/Zambia_ExtendedEB_1990-.csv"
 
-##
 
+# Input the raw data from esnon
+
+
+#
 get_unit_esnon <- function(InputData){
   
   listVal <- InputData
@@ -151,6 +73,7 @@ get_unit_esnon <- function(InputData){
   
 }
 
+# 
 get_processed_esnon_data <- function(InputData){
   
   dataVal <- InputData
@@ -169,81 +92,206 @@ get_processed_esnon_data <- function(InputData){
   
   listVal <- dataVal$Product
   listVal2 <- listVal[!is.na(listVal)]
-
+  
   listVal = as.character(lapply(listVal2, get_unit_esnon))
   
   dataVal2 <- as_tibble(dataVal)  %>% 
     dplyr::bind_cols(unit = listVal) %>%
     dplyr::select(source, product, flow, stage, unit, year, value) # %>%
-    # tidyr::spread(key = year, value = value)
-
+  # tidyr::spread(key = year, value = value)
+  
   
 }
 
 
-df_esnon2 <- get_processed_esnon_data(df)
-df_esnon2
+# df_esnon2 <- get_processed_esnon_data(df)
+# df_esnon2
 
 
-# df2 = df_oed_prod$product_esnon %>% 
-#   dplyr::mutate(unit = ifesle(str_detect(product_esnon, "\\(kt\\)") == T,
-#                               "kt", "nothings"))
+# Combine the data into one dataframe
+combine_esnon_data <- function(InputDataPath){
+  
+  dataVal_flow <- df_oed_flow %>% 
+    filter(!is.na(flow_esnon))
+  
+  file_path <- InputDataPath
+  
+  sheets <- excel_sheets(path = file_path)
+  
+  dataVal_mega <- NULL
+  
+  for (sheet in sheets){
+    
+    dataVal <- readxl::read_xlsx(file_path,
+                                 sheet = sheet,
+                                 skip = 6,
+                                 col_names = T) %>%
+      dplyr::filter(PRODUCT %in% df_oed_flow$flow_esnon,
+                    !is.na(PRODUCT)) %>%
+      dplyr::mutate(source = "esnon", # include the source of the data
+                    year = as.numeric(sheet),
+                    flow_index = dataVal_flow$flow_esnon_eeb_index) %>% 
+      tidyr::gather("Product", "value", -PRODUCT, -source, -year, -flow_index) %>% 
+      dplyr::mutate(value = as.numeric(value))
+      
+    dataVal <- get_processed_esnon_data(dataVal)
+    
+    dataVal_mega = dplyr::bind_rows(dataVal_mega, dataVal)
+    
+  }
+  
+  return(dataVal_mega)
+  
+}
+
+df_esnon <- combine_esnon_data(esnon_file)
+df_esnon
+
+
+
+# length(unique(df_esnon$PRODUCT))
+# length(unique(df_oed_flow$flow_esnon))
 # 
-# df2
 # 
-# unit = ifelse(str_detect(df_oed_prod$product_esnon, "\\(kt\\)") == T,
-#               "kt", "nothings")
+# dfx1 <- df_oed_flow %>% 
+#   filter(!is.na(flow_esnon))
 # 
+# 
+# df_esnon$PRODUCT == dfx1$flow_esnon
+# 
+# df <- df_esnon %>% 
+#   tidyr::gather("Product", "value", -PRODUCT, -source, -year, -flow_index)
+#   
+# df
+
+
+
+#Input the raw data
+# df_eeb <- read_csv(eeb_file,
+#                    show_col_types = FALSE,
+#                    col_names = T) %>%
+#   dplyr::mutate(source = "eeb") # Include the source of the data
+# 
+# df_eeb
+# 
+# 
+# unique(df_eeb$'Flag Codes')
+# unique(df_eeb$Unit)
+# 
+# length(sort(unique(df_eeb$Product))) == length(sort(unique(df_eeb$Flow)))
+
+
+## get the names of the products in the dataset
+# df_raw %>% 
+#   dplyr::select(PRODUCT, Product) %>% 
+#   dplyr::distinct() -> df_prod
+# 
+# write.csv(df_prod, 'eb_products.csv', row.names = F)
+
+
+## get the names of the flow in the dataset
+# df_raw %>%
+#   dplyr::select(FLOW, Flow) %>%
+#   dplyr::distinct() -> df_flow
+# 
+# write.csv(df_flow, 'eb_flow.csv', row.names = F)
+
+# 
+# # Input other data that will be used to process the energy balance
+# df_oed_flow <- readxl::read_xlsx("other_energy_data.xlsx",
+#                              sheet = "flow",
+#                              col_names = T) %>% 
+#   dplyr::distinct()
+# 
+# df_oed_flow
+# 
+# # length(sort(unique(df_oed_flow$Flow)))
+# 
+# 
+# df_oed_prod <- readxl::read_xlsx("other_energy_data.xlsx",
+#                              sheet = "product",
+#                              col_names = T) %>%
+#   dplyr::distinct()
+# 
+# df_oed_prod
 # # 
-# # get_unit_esnon <- function(InputData){
-# #   
-# #   listVal <- InputData
-# #   # print((listVal))
-# #   # listVal2 <- listVal[!is.na(listVal)]
-# #   # print((listVal2))
-# #   
-# #   if(str_detect(listVal, "\\(kt\\)") == T){
-# #     "kt"
-# #     } else if(str_detect(listVal, "\\(TJ\\)") == T){
-# #       "TJ"
-# #     } else   if(str_detect(listVal, "\\(GWh\\)") == T){
-# #       "GWh"
-# #     } else if(str_detect(listVal, "\\(TJ-net\\)") == T){
-# #       "TJ net"
-# #     } else if(str_detect(listVal, "\\(TJ-gross\\)") == T){
-# #       "TJ gross"
-# #     } else if(str_detect(listVal, "\\(direct use in TJ-net\\)") == T){
-# #       "TJ net"
-# #     } else {
-# #       ""
-# #     }
-# #   
-# # }
-# 
-# # x = c("Fuel oil (kt)", "Other recovered gases (TJ-gross)", "Heat (TJ)", "Source")
-# # 
-# # class(x)
-# # x1 = !is.na(df_oed_prod$product_esnon)
-# 
-# lst_product_esnon <- df_oed_prod$product_esnon
-# lst_product_esnon2 <- lst_product_esnon[!is.na(lst_product_esnon)]
-# # lst_product_esnon2
-# # class(x1)
-# # 
-# # # xunit <- get_unit_esnon(df_oed_prod$product_esnon)
-# # lapply(x, get_unit_esnon)
-# # x1 = as.character(lapply(lst_product_esnon2, get_unit_esnon))
-# # x1
-# 
-# x1a = as.character(lapply(lst_product_esnon, get_unit_esnon))
-# x1a
-##
+# # length(sort(unique(df_oed_prod$Flow)))
 
 
 
-get_processed_eeb_data <- function(InputData, InputYear1, InputYear2){
+
+# ##
+# 
+# get_unit_esnon <- function(InputData){
+#   
+#   listVal <- InputData
+#   
+#   if(str_detect(listVal, "\\(kt\\)") == T){
+#     "kt"
+#   } else if(str_detect(listVal, "\\(TJ\\)") == T){
+#     "TJ"
+#   } else   if(str_detect(listVal, "\\(GWh\\)") == T){
+#     "GWh"
+#   } else if(str_detect(listVal, "\\(TJ-net\\)") == T){
+#     "TJ net"
+#   } else if(str_detect(listVal, "\\(TJ-gross\\)") == T){
+#     "TJ gross"
+#   } else if(str_detect(listVal, "\\(direct use in TJ-net\\)") == T){
+#     "TJ net"
+#   } else {
+#     ""
+#   }
+#   
+# }
+# 
+# get_processed_esnon_data <- function(InputData){
+#   
+#   dataVal <- InputData
+#   
+#   dataVal$product <- plyr::mapvalues(dataVal$Product,
+#                                      from = df_oed_prod$product_esnon,
+#                                      to = df_oed_prod$product_common)
+#   
+#   dataVal$flow <- plyr::mapvalues(dataVal$flow_index,
+#                                   from = df_oed_flow$flow_esnon_eeb_index,
+#                                   to = df_oed_flow$flow_common)
+#   
+#   dataVal$stage <- plyr::mapvalues(dataVal$flow_index,
+#                                    from = df_oed_flow$flow_esnon_eeb_index,
+#                                    to = df_oed_flow$Category2)
+#   
+#   listVal <- dataVal$Product
+#   listVal2 <- listVal[!is.na(listVal)]
+# 
+#   listVal = as.character(lapply(listVal2, get_unit_esnon))
+#   
+#   dataVal2 <- as_tibble(dataVal)  %>% 
+#     dplyr::bind_cols(unit = listVal) %>%
+#     dplyr::select(source, product, flow, stage, unit, year, value) # %>%
+#     # tidyr::spread(key = year, value = value)
+# 
+#   
+# }
+# 
+# 
+# df_esnon2 <- get_processed_esnon_data(df)
+# df_esnon2
+# 
+# 
+# ##
+
+
+
+get_processed_eeb_data <- function(InputData){ #, InputYear1, InputYear2
   
   dataVal <- InputData
+  
+  dataVal <- read_csv(InputData,
+                     show_col_types = FALSE,
+                     col_names = T) %>%
+    dplyr::mutate(source = "eeb") # Include the source of the data
+  
+  # df_eeb
   #
   # print(dim(dataVal))
   
@@ -270,50 +318,86 @@ get_processed_eeb_data <- function(InputData, InputYear1, InputYear2){
     dplyr::rename(year = Time,
                   unit = Unit,
                   value = Value) %>%
-    dplyr::select(source, product, flow, stage, unit, year, value) %>%
-    dplyr::filter(year >= InputYear1 & year <= InputYear2,
-                  # Flow == "Production"
-    ) #%>%
+    dplyr::select(source, product, flow, stage, unit, year, value) #%>%
+    # dplyr::filter(year >= InputYear1 & year <= InputYear2,
+    #               # Flow == "Production"
+    # ) #%>%
     # tidyr::spread(key = year, value = value) %>%
     # as_tibble()
   
 }
 
 
-df_eeb2 <- get_processed_eeb_data(df_eeb, 2008, 2010)
-df_eeb2
+df_eeb <- get_processed_eeb_data(eeb_file)
+df_eeb
 
 
-unique(df_eeb2$product)
+df_combined <- dplyr::bind_rows(df_esnon, df_eeb) %>% 
+  dplyr::full_join(df_conv_factors, by = c('product', 'unit')) %>% 
+  dplyr::mutate(value_TJ = value * unit_value_in_TJ)
 
-dfx = df_eeb2 %>% 
-  filter(stage == "Final consumption",
-         product == #"Electricity"
-         "Gas/diesel oil excl. biofuels" )
-  # tidyr::spread(key = year, value = value)
+df_combined
 
-dfx
-
-df_eeb2 <- df_eeb %>% 
-  dplyr::select(Product, Flow, Unit, Time, Value) %>% 
-  dplyr::filter(Time > 2007 & Time < 2012,
-                # Flow == "Production"
-                ) %>%
-  dplyr::mutate(Source = "eeb") %>% 
-  tidyr::spread(key = Time, value = Value)
-
-df_eeb2
+unique(df_combined$product)
 
 
-df_eeb2$product <- plyr::mapvalues(df_eeb2$Product,
-                                   from = df_oed_prod$products_eeb2, 
-                                   to = df_oed_prod$product_common)
+
+# df_units <- df_combined %>% 
+#   dplyr::select(product, unit) %>% 
+#   dplyr::distinct() %>% 
+#   dplyr::filter(unit != 'TJ')
+# 
+# df_units
+# 
+# # write.csv(df_units, 'units_factors.csv', row.names = F)
+
+# df_units2 <- df_combined %>%
+#   dplyr::select(product, unit) %>%
+#   dplyr::distinct() %>%
+#   dplyr::filter(!unit %in% c('TJ gross', 'GWh', 'kt'))
+# 
+# df_units2
+# 
+# # write.csv(df_units2, 'units_factors2.csv', row.names = F)
 
 
-df_eeb2$flow <- plyr::mapvalues(df_eeb2$Flow,
-                                   from = df_oed_flow$flow_eeb2, 
-                                   to = df_oed_flow$flow_common)
+# Get the series in a specified range of years
+get_series <- function(InputData, InputYear1, InputYear2){
+  
+  dataVal <- InputData %>%
+    dplyr::filter(year >= InputYear1 & year <= InputYear2,
+                  # Flow == "Production",
+                  stage ==  "Final consumption"
+                  ) #%>%
+  
+}
+
+df <- get_series(df_combined, 1985, 2020) %>% 
+  dplyr::arrange(year, product) %>% 
+  dplyr::filter(flow == "Final consumption",
+                # product == #"Electricity"
+                #   # "Hydro"
+                #   # "Gas/diesel oil excl. biofuels"
+                #   # "Fuel oil"
+                #   "Liquefied petroleum gases (LPG)"
+                )
+
+df
 
 
-unique(df_eeb2$product)
+# Compare the two data sources
+df %>% 
+  filter(value_TJ > 0,
+         !product %in% c("Total",
+                        "Memo: Renewables")) %>% 
+  ggplot2::ggplot(aes(year, value_TJ/1e3, colour = source)) +
+  facet_wrap(~ product, 
+             nrow = 2) + 
+  geom_line() +
+  xlab("Years") +
+  ylab("Final Consumption in PJ") +
+  theme(legend.position = "bottom") -> plt_all
 
+plt_all
+
+## The script ends here ##
